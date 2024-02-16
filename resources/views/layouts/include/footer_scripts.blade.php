e7053082157d901666dad9a4fe049cb096d04ab4
mason.hows11 <mason.hows11@gmail.com>
2024-02-16 19:09:26 +0330

    first commit

---
+++ resources/views/layouts/include/footer_scripts.blade.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+
+
---
+++ resources/views/layouts/include/master_front.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{ asset('front/image/icon.png') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('page_title')</title>
+    @include('front.layouts.header_styles')
+</head>
+<body>
+<!-- start header -->
+@include('front.layouts.include.header')
+<!-- end header -->
+
+<!-- start responsive header-->
+@include('front.layouts.include.header_responsive')
+<!-- end responsive header-->
+
+<!-- start main menu -->
+@include('front.layouts.include.nav')
+<!-- end main menu -->
+
+<!-- start main -->
+<main>
+    @yield('main_content')
+</main>
+<!-- end main -->
+
+<!-- start footer -->
+@include('front.layouts.include.footer')
+<!-- end footer -->
+
+@include('front.layouts.footer_scripts' )
+@include('front.layouts.alert.delete_confirm',['className'=> 'delete-item'])
+@include('front.layouts.alert.alert')
+@stack('front_custom_scripts')
+</body>
+
+</html>

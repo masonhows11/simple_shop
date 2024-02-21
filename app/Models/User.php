9cd6a8b71a53d3d0440fb82b8126413fddf44a01
mason.hows11 <mason.hows11@gmail.com>
2024-02-21 20:47:11 +0330

    add route for verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Services\Permission\Traits\HasPermission;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable,HasPermission;
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Front\Auth\LoginController;
 use App\Http\Controllers\Front\Auth\ProfileController;
 use App\Http\Controllers\Front\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,6 +35,28 @@ Route::prefix('auth')->name('auth.')->group(function (){
     Route::get('/register',[RegisterController::class,'registerForm'])->name('register.form');
     Route::post('/register',[RegisterController::class,'register'])->name('register');
 
+
+    /////// for verified user email ///////
+
+    //// The Email Verification Notice
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->middleware('auth')->name('verification.notice');
+    
+    //// The Email Verification Handler
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect('/home');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
+
+    //// Resending the Verification Email
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 });
 
 
@@ -40,7 +64,7 @@ Route::prefix('auth')->name('auth.')->group(function (){
 // logged in route
 Route::group(['middleware'=>'web'],function (){
 
-    Route::get('/profile',[ProfileController::class,'profile'])->name('profile');
+    Route::get('/profile',[ProfileController::class,'profile'])->name('profile')->middleware(['verified']);
     Route::get('/log-out',[LoginController::class,'logOut'])->name('log.out');
 
 });

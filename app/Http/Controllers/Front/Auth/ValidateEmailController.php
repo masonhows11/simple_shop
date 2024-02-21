a32ae4cc0763b02fc2ac2352a665faaef560a4a3
mason.hows11 <mason.hows11@gmail.com>
2024-02-21 23:16:42 +0330

    add controller for verify email

---
+++ app/Http/Controllers/Front/Auth/ValidateEmailController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Front\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ValidateEmailController extends Controller
+{
+    //
+
+
+    public function sendEmailVerification()
+    {
+        
+    }
+    
+    
+    public function verifyEmailVerification(){
+        
+    }
+
+
+    public function resendEmailVerification()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -35,29 +35,28 @@ Route::prefix('auth')->name('auth.')->group(function (){
     Route::get('/register',[RegisterController::class,'registerForm'])->name('register.form');
     Route::post('/register',[RegisterController::class,'register'])->name('register');
 
-
-    /////// for verified user email ///////
-
-    //// The Email Verification Notice
-    Route::get('/email/verify', function () {
-        return view('auth.verify-email');
-    })->middleware('auth')->name('verification.notice');
-
-    //// The Email Verification Handler
-    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-
-        return redirect('/home');
-    })->middleware(['auth', 'signed'])->name('verification.verify');
-
-    //// Resending the Verification Email
-    Route::post('/email/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-
-        return back()->with('message', 'Verification link sent!');
-    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 });
+
+/////// for verified user email ///////
+
+//// The Email Verification Notice
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+//// The Email Verification Handler
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+//// Resending the Verification Email
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
 

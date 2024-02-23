5e4dddab55f50be8f48443ad891288ad35e42737
mason.hows11 <mason.hows11@gmail.com>
2024-02-23 11:57:27 +0330

    add reset pass function codes

--- app/Http/Controllers/Front/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Front/Auth/ForgotPasswordController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Front\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\NewPasswordRequest;
 use App\Http\Requests\ResetPasswordRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class ForgotPasswordController extends Controller
 {
@@ -50,7 +53,23 @@ class ForgotPasswordController extends Controller
 
     public function resetPassword(NewPasswordRequest $request)
     {
-        dd($request->all());
+       // dd($request->all());
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+               // event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
     }
 
 

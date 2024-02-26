3c234184c8e7946e6822444ad90ac00a63d39b82
mason.hows11 <mason.hows11@gmail.com>
2024-02-26 21:45:13 +0330

    implement cart updated

---
+++ app/Services/Basket/Basket.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Services\Basket;
+
+
+use App\Models\Product;
+use App\Services\Storage\Contracts\StorageInterface;
+
+class Basket
+{
+
+    private $storage;
+
+    public function __construct(StorageInterface $storage)
+    {
+            $this->storage = $storage;
+    }
+
+    public function addToBasket(Product $product,int $quantity)
+    {
+            $this->storage->set($product->id,['quantity' => $quantity ]);
+    }
+
+}

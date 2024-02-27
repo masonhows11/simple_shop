5fbf493042a2f9dd397a3c96e87a7d3fb1186031
mason.hows11 <mason.hows11@gmail.com>
2024-02-27 19:00:15 +0330

    cart design view updated

--- resources/views/front/cart/cart.blade.php
+++ resources/views/front/cart/cart.blade.php
@@ -11,7 +11,43 @@
                 @include('layouts.alert.alert')
             </div>
         </div>
-        <div class="row ">
+        <div class="row d-flex justify-content-between ">
+
+            <div class="col-lg-7 mt-4 bg-secondary-subtle py-2">
+                <table class="table table-secondary mt-2">
+                    <thead>
+                    <tr>
+                        <th>{{ __('messages.product_name') }}</th>
+                        <th>{{ __('messages.product_price') }}</th>
+                        <th>{{ __('messages.product_quantity') }}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+            <div class="col-lg-4 mt-4 bg-secondary-subtle py-4">
+                <h4 class="mt-4 h4">{{ __('messages.payment') }}</h4>
+                <hr>
+                <div>{{ __('messages.total_price') }}</div>
+                <hr>
+                <div>{{ __('messages.shipment_price') }}</div>
+                <hr>
+                <div>{{ __('messages.the_amount_payable') }}</div>
+                <hr>
+                <div>
+                    <form action="">
+                        <button class="btn btn-primary w-100" type="submit">{{ __('messages.register_and_pay') }}</button>
+                    </form>
+                </div>
+            </div>
 
 
         </div>

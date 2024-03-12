31bbf05643a58470bb99c0287356ce7e3c4fd867
mason.hows11 <mason.hows11@gmail.com>
2024-03-12 19:16:54 +0330

    problem clear session after bank callback not solved yet

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
@@ -197,7 +197,6 @@ return [
     */
 
     'same_site' => 'lax',
-    //  'same_site' => null,
 
     /*
     |--------------------------------------------------------------------------

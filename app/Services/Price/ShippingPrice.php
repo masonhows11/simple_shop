0914d0b4362f8dc2ae93da47a9fc5965e004e9fd
mason.hows11 <mason.hows11@gmail.com>
2024-03-16 18:20:22 +0330

    combine the shipping price with cart price updated

--- app/Services/Price/ShippingPrice.php
+++ app/Services/Price/ShippingPrice.php
@@ -11,28 +11,33 @@ class ShippingPrice implements PriceInterface
     private $price;
     const SHIPPING_COST = 20000;
 
+    ////  when PriceInterface $price is call
+    //// basketPrice is returned then we used it
     public function __construct(PriceInterface $price)
     {
+        //// in price variable we have previous price values from basketPrice
         $this->price = $price;
     }
 
     public function getPrice()
     {
-      return  self::SHIPPING_COST;
+        return self::SHIPPING_COST;
     }
 
     public function getTotalPrices()
     {
+        //// price->getTotalPrices() is from basketPrice
         return $this->price->getTotalPrices() + $this->getPrice();
     }
 
     public function persianDescription()
     {
-       return 'هزینه حمل';
+        return 'هزینه حمل';
     }
 
     public function getSummary()
     {
-       return array_merge($this->price->getSummary(),[$this->persianDescription() => $this->getPrice()]);
+        //// merge info into array for display in summary view
+        return array_merge($this->price->getSummary(), [$this->persianDescription() => $this->getPrice()]);
     }
 }

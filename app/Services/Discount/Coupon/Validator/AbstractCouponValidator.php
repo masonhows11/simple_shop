16d89fe2f20b423e09927428cb8f2d4f4c5d82d5
mason.hows11 <mason.hows11@gmail.com>
2024-03-17 10:23:06 +0330

    add comment for our validator desing pattern

--- app/Services/Discount/Coupon/Validator/AbstractCouponValidator.php
+++ app/Services/Discount/Coupon/Validator/AbstractCouponValidator.php
@@ -12,12 +12,18 @@ abstract class AbstractCouponValidator implements CouponValidatorInterface
     //// for set next validator
     private $nextValidator;
 
-    //// set next validate function
+    //// set next validate function for validate coupon code
     public function setNextValidator(CouponValidatorInterface $couponValidator){
 
+        $this->nextValidator = $couponValidator;
     }
 
     public function validate(Coupon $coupon){
 
+        //// the chain of validator is end
+        /// and there is not next validator function
+        if($this->nextValidator == null){
+            return true;
+        }
     }
 }

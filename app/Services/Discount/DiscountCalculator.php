fdadc66430b125bd1329c3060d8a675437a2e85c
mason.hows11 <mason.hows11@gmail.com>
2024-03-17 19:03:42 +0330

    calculate discount updated 90%

--- app/Services/Discount/DiscountCalculator.php
+++ app/Services/Discount/DiscountCalculator.php
@@ -11,11 +11,10 @@ class DiscountCalculator
 
     public function discountAmount(Coupon $coupon, int $amount)
     {
-        // dd($amount);
+
         $discountAmount = (int)(($coupon->percent / 100) * $amount);
-       // dd($this->isExceeded($discountAmount,$coupon->limit));
         return $this->isExceeded($discountAmount,$coupon->limit) ? $coupon->limit  : $discountAmount;
-        // dd($discountAmount);
+
     }
 
     private function isExceeded(int $amount, int $limit)

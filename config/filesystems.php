f3e3c3d6a776288b76450f046dc3cbeef4ab8ac0
mason.hows11 <mason.hows11@gmail.com>
2024-03-23 16:52:32 +0330

    add private disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,10 +36,18 @@ return [
             'throw' => false,
         ],
 
+        //// new custom disk as private access not public access 
+        'private' => [
+
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+
+
+        ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],

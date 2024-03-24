bb61cbd0d2e19c93a8335126e1925535e505f873
mason.hows11 <mason.hows11@gmail.com>
2024-03-24 06:13:05 +0330

    add footer for theme project

---
+++ resources/views/layouts/include/footer.blade.php
@@ -0,0 +1,62 @@
+<footer class="border-top mt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-sm-6 footer-box">
+                <p>با گرافیک شاپ </p>
+                <ul class="ps-0">
+                    <li class="mb-3"><a href="{{ route('not.found') }}">اتاق خبر گرافیک شاپ</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">فرصت های شغلی</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">تماس با گرافیک شاپ</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">درباره گرافیک شاپ</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-sm-6 footer-box">
+                <p>خدمات مشتریان</p>
+                <ul class="ps-0">
+                    <li class="mb-3"><a href="{{ route('not.found') }}">پرسش‌های متداول</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">شرایط استفاده</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">حریم خصوصی</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">گزارش تخلف</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 footer-box">
+                <p>راهنمای خرید از گرافیک شاپ</p>
+                <ul class="ps-0">
+                    <li class="mb-3"><a href="{{ route('not.found') }}">نحوه ثبت سفارش</a></li>
+                    <li class="mb-3"><a href="{{ route('not.found') }}">شیوه‌های پرداخت</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-5 col-sm-6 footer-box">
+                <p class="mb-4">همراه ما باشید!</p>
+                <div class="social-list">
+                    <a href="#"><i class="fab fa-instagram social-media"></i></a>
+                    <a href="#"><i class="fab fa-telegram social-media"></i></a>
+                    <a href="#"><i class="fab fa-linkedin social-media"></i></a>
+                    <a href="#"><i class="fab fa-twitter social-media"></i></a>
+                </div>
+                <p>با ثبت ایمیل، از جدید‌ترین تخفیف‌ها با‌خبر شوید</p>
+                <div class="input-group mt-4 mb-5 email-box">
+                    <input type="email" class="form-control form-control-lg" placeholder="ایمیل شما">
+                    <button type="submit" class="btn btn-danger font-13 px-3">ارسال</button>
+                </div>
+            </div>
+        </div><!-- end footer box -->
+        <div class="row"><!-- start footer details -->
+            <div class="col-md-8 col-12 footer-details ps-0">
+                <p>فروشگاه اینترنتی گرافیک شاپ ، بررسی، انتخاب و خرید آنلاین</p>
+            </div>
+            <div class="col-md-4 col-12 d-flex align-items-center justify-content-end footer-pic pe-0">
+                <img src="{{ asset('front_assets/images/f-1.png') }}" class="footer-detail-pic">
+                <img src="{{ asset('front_assets/images/f-2.png') }}" class="footer-detail-pic">
+            </div>
+        </div><!-- end footer details -->
+        <div class="row"><!-- start copy right -->
+            <div class="col-12 copy-right">
+                <p>استفاده از مطالب فروشگاه اینترنتی گرافیک شاپ فقط برای مقاصد غیرتجاری و
+                    با ذکر منبع بلامانع است. کلیه حقوق این سایت متعلق به یاس دیزاین می‌باشد.
+                </p>
+            </div>
+        </div><!-- end copy right -->
+    </div>
+    <a href="#" class="topbutton"><i class="fa fa-chevron-up"></i></a><!-- top button -->
+</footer>

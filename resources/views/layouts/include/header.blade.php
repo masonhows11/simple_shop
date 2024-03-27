a8e00bc4f54ac80e344254bae7cae19fb4793a50
mason.hows11 <mason.hows11@gmail.com>
2024-03-27 06:46:39 +0330

    header auth user items style updated

--- resources/views/layouts/include/header.blade.php
+++ resources/views/layouts/include/header.blade.php
@@ -1,50 +1,71 @@
 {{-- for inject  class into blade & use it --}}
-@inject('basket','App\Services\Basket\Basket')
+@inject('basket', 'App\Services\Basket\Basket')
 <nav class="navbar navbar-expand-lg bg-dark-subtle bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('home') }}">{{ __('messages.site_name') }}</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('home') }}">{{ __('messages.home_page') }}</a>
+                    <a class="nav-link" aria-current="page"
+                        href="{{ route('home') }}">{{ __('messages.home_page') }}</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('products') }}">{{ __('messages.products') }}</a>
                 </li>
 
                 @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cart') }}">
-                            {{ __('messages.basket') }}  <span class="badge text-bg-primary">{{ $basket->itemCount() }}</span>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            {{ \Illuminate\Support\Facades\Auth::user()->name }}
                         </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('profile') }}">{{__('messages.profile')}}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('ticket.index') }}">{{__('messages.tickets')}}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('orders.index') }}">{{__('messages.orders')}}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('profile') }}">{{ \Illuminate\Support\Facades\Auth::user()->name  }}</a>
+                        <ul class="dropdown-menu bg-dark-subtle bg-body-tertiary">
+                            <li><a class="dropdown-item mt-2" href="{{ route('profile') }}">{{ __('messages.profile') }}</a>
+                            </li>
+                            <li><a class="dropdown-item mt-2"
+                                    href="{{ route('ticket.index') }}">{{ __('messages.tickets') }}</a></li>
+                            <li><a class="dropdown-item mt-2"
+                                    href="{{ route('orders.index') }}">{{ __('messages.orders') }}</a></li>
+                        </ul>
                     </li>
 
-                   {{--  @role('admin')
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('cart') }}">
+                            {{ __('messages.basket') }} <span
+                                class="badge text-bg-primary">{{ $basket->itemCount() }}</span>
+                        </a>
+                    </li>
+
+                    {{--  <li class="nav-item">
+                        <a class="nav-link" href="{{ route('profile') }}">{{ __('messages.profile') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('ticket.index') }}">{{ __('messages.tickets') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('orders.index') }}">{{ __('messages.orders') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="{{ route('profile') }}">{{ \Illuminate\Support\Facades\Auth::user()->name }}</a>
+                    </li>  --}}
+
+                    {{--  @role('admin')
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('admin.index') }}">{{ __('messages.admin_dashboard') }}</a>
                     </li>
-                    @endrole--}}
+                    @endrole --}}
 
                     {{-- if u want use @can directive u can define a gate --}}
                     @can('show_panel')
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.index') }}">{{ __('messages.admin_dashboard') }}</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.index') }}">{{ __('messages.admin_dashboard') }}</a>
+                        </li>
                     @endcan
 
 
@@ -53,12 +74,13 @@
                         <a class="nav-link" href="{{ route('log.out') }}">{{ __('messages.log_out') }}</a>
                     </li>
                 @else
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('auth.login.form') }}">{{__('messages.login')}}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('auth.register.form') }}">{{ __('messages.register_user') }}</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('auth.login.form') }}">{{ __('messages.login') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="{{ route('auth.register.form') }}">{{ __('messages.register_user') }}</a>
+                    </li>
                 @endauth
             </ul>
         </div>
